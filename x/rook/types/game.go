2960132b6505a2d81eb18b3ed63dd2603e8210ff
Callum <cmwaters19@gmail.com>
2020-10-24 00:32:42 +0200

    add logic to validate building a settlement

--- x/rook/types/game.go
+++ x/rook/types/game.go
@@ -1,6 +1,7 @@
 package types
 
 import (
+	"errors"
 	"fmt"
 	// "math/rand"
 
@@ -59,10 +60,10 @@ func (g *GameState) PopulateFactions() {
 func (g *GameState) Build(faction *Faction, settlement Settlement, position *Position) error {
 	// validate position
 	if position.X >= g.Config.Map.Width {
-		return fmt.Errorf("Position X (%d) exceeds game boundaries (%d)", position.X, g.Config.Map.Width)
+		return fmt.Errorf("position X (%d) exceeds game boundaries (%d)", position.X, g.Config.Map.Width)
 	}
 	if position.Y >= g.Config.Map.Height {
-		return fmt.Errorf("Position Y (%d) exceeds game boundaries (%d)", position.Y, g.Config.Map.Height)
+		return fmt.Errorf("position Y (%d) exceeds game boundaries (%d)", position.Y, g.Config.Map.Height)
 	}
 
 	// check that the faction occupies this piece of land
@@ -73,15 +74,75 @@ func (g *GameState) Build(faction *Faction, settlement Settlement, position *Pos
 
 	// check that the faction has the allocated resources
 	if !ConstructionResources(g.Config.Construction, settlement).Less(faction.Resources) {
-		return fmt.Errorf("Not enough resources. Required: %s, got: %s",
+		return fmt.Errorf("not enough resources. Required: %s, got: %s",
 			ConstructionResources(g.Config.Construction, settlement), faction.Resources)
 	}
+
+	// check all the settlement specific rules
+	// lumbermills must be on a forest
+	if settlement == Settlement_LUMBERMILL && g.Map[position.X][position.Y].Landscape != Landscape_FOREST {
+		return errors.New("building a lumbermill must be over a forest")
+	}
+
+	// A quarry must be built adjacent to at least one mountain range
+	isQuarry := func (tile *Tile) bool { return tile.Landscape == Landscape_MOUNTAINS}
+	if settlement == Settlement_QUARRY && len(g.searchAdjacent(position, isQuarry)) == 0 {
+		return errors.New("building a quarry must be adjacent to mountains")
+	}
+
+	// other settlements can't be in a forest
+	if settlement != Settlement_LUMBERMILL && g.Map[position.X][position.Y].Landscape != Landscape_FOREST {
+		return fmt.Errorf("building a %v can't be in a forest", settlement)
+	}
+
+	// we are good to build the settlement
+	faction.Settlements[position.Index(g.Config.Map)] = settlement
 
 	return nil
 }
 
 func (g *GameState) Move(faction *Faction, quantity uint32, origin *Position, direction Direction) error {
 	return nil
+}
+
+var directions = []Direction{Direction_LEFT, Direction_RIGHT, Direction_UP, Direction_DOWN}
+
+func (g *GameState) searchAdjacent(position *Position, condition func(*Tile) bool) []*Tile {
+	tiles := make([]*Tile, 0, 4)
+	for _, direction := range directions {
+		tile := g.neighborTile(position, direction)
+		if condition(tile) {
+			tiles = append(tiles, tile)
+		}
+	}
+	return tiles
+}
+
+func (g *GameState) neighborTile(position *Position, direction Direction) *Tile {
+	switch direction {
+	case Direction_LEFT:
+		if position.X == 0 {
+			return &g.Map[g.Config.Map.Width - 1][position.Y]
+		}
+		return &g.Map[position.X - 1][position.Y]
+	case Direction_RIGHT:
+		if position.X == g.Config.Map.Width - 1 {
+			return &g.Map[0][position.Y]
+		}
+		return &g.Map[position.X + 1][position.Y]
+	case Direction_UP:
+		if position.Y == g.Config.Map.Height - 1 {
+			return &g.Map[position.X][0]
+		}
+		return &g.Map[position.X][position.Y + 1]
+	case Direction_DOWN:
+		if position.X == 0 {
+			return &g.Map[position.X][g.Config.Map.Height - 1]
+		}
+		return &g.Map[position.X][position.Y - 1]
+	default: //unknown direction
+		return nil
+	}
 }
 
 type PendingGameState struct {
---
+++ x/rook/types/primitives.go
@@ -0,0 +1,15 @@
+package types
+
+
+func (p *Position) Sum(p2 *Position) *Position {
+	return &Position{X: p.X + p2.X, Y: p.Y + p2.Y}
+}
+
+func (p *Position) Wrap(c *MapConfig) {
+	p.X = p.X % c.Width
+	p.Y = p.Y % c.Height
+}
+
+func (p *Position) Index(c *MapConfig) uint32 {
+	
+}
